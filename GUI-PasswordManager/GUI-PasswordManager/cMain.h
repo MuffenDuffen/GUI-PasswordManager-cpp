a81eb9b1e4a015712589c17c1aecb8d5433fbbeb
MuffenDuffen <50796186+MuffenDuffen@users.noreply.github.com>
2021-09-09 15:54:08 +0200

    Revert "added more gui and a filer"
    This reverts commit a173d57e77accc77e3dd0f22c9673dfae10c118e.

--- GUI-PasswordManager/GUI-PasswordManager/cMain.h
+++ GUI-PasswordManager/GUI-PasswordManager/cMain.h
@@ -1,20 +1,12 @@
 ﻿#pragma once
-#include "cFiler.h"
 #include "wx/wx.h"
 
 class cMain : public wxFrame
 {
 public:
     cMain();
-    ~cMain() override;
-
-	void OnPassEnter(wxCommandEvent& event);
+    ~cMain();
 	
 public:
-	wxStaticText* Text;
-	wxTextCtrl* Input;
-
-	cFiler* Filer;
-
-	wxDECLARE_EVENT_TABLE();
+	wxStaticText* text;
 };
